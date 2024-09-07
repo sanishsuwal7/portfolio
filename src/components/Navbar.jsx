65d86454eda45bc02b45551dbf9ce70a5aa6e64f
Sanish Suwal <sanishsuwal3@gmail.com>
2024-09-07 13:12:56 -0400

    asdasd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <p className="mx-2 w-10 text-lg font-bold tracking-tight lg:mt-8 lg:text-3xl">Ss</p>
+            <p className="mx-2 w-10 font-bold tracking-tight lg:mt-8 lg:text-3xl">Ss</p>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <a href={`https://www.linkedin.com/in/sanishsuwal`} target="blank">
