b8581d2c8575543d63dfecb4d09d544ac9b6505f
Sanish Suwal <sanishsuwal3@gmail.com>
2024-09-07 13:10:55 -0400

    did some stuff with logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/logo.png";
 import {FaGithub, FaLinkedin, FaEnvelope} from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-10" src={logo} alt="logo" />
+            <p className="mx-2 w-10 text-lg font-bold tracking-tight lg:mt-8 lg:text-3xl">Ss</p>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <a href={`https://www.linkedin.com/in/sanishsuwal`} target="blank">
