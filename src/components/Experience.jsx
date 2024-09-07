5ee3792c57046687026ce11dab83a678f3162f50
Sanish Suwal <sanishsuwal3@gmail.com>
2024-09-07 14:08:28 -0400

    changed wrap

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -23,7 +23,7 @@ const Experience = () => {
                   whileInView={{opacity: 1, x:0}}
                   initial={{opacity: 0, x:100}}
                   transition={{duration: 1}}
-                  className="w-full max-w-xl lg:w-3/4">
+                  className="flex flex-wrap w-full max-w-xl lg:w-3/4">
                     <h6 className="mb-2 font-semibold "> {experience.role} - {" "}
                         <span className="text-sm text-purple-100">{experience.company}</span>
                     </h6>
