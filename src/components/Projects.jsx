311ad7ce330c1d1c34b286af81028fac42430197
Sanish Suwal <sanishsuwal3@gmail.com>
2025-01-09 13:13:33 -0500

    updated live demo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,9 +2,7 @@ import { PROJECTS } from "../constants"
 import {animate, motion} from 'framer-motion';
 
 const Projects = () => {
-    const handleClick = () => {
-        window.location.href = "https://chatapp-t58c.onrender.com/";
-    }
+   
 
   return (
     <div className="border-b border-neutral-900 pb-4">
@@ -16,7 +14,7 @@ const Projects = () => {
         </motion.h1>
         <div>
             {PROJECTS.map((project, index) => (
-                <div key={index} className="mb-8 flex flex-wrap lg:justify-evenly">
+                <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
                     <motion.div 
                          whileInView={{opacity: 1, x:0}}
                          initial={{opacity: 0, x:-100}}
@@ -29,19 +27,18 @@ const Projects = () => {
                          initial={{opacity: 0, x:100}}
                          transition={{duration: 1}}
                         className="flex flex-wrap w-full max-w-xl lg:w-3/4">
-                            <div className="flex flex-row w-full justify-items-start">
-                                <h6 className="mb-3 font-semibold">{project.title}</h6>
-                                {project.title === "Real-time Chat Application" && (
-                                    <button className="mx-4 rounded bg-neutral-900 px-2 py-1 text-small font-medium text-purple-900" onClick={handleClick}>
-                                        Live Demo
-                                    </button>
-                                 )}
-                            </div>
-                        
+                        <h6 className="mb-3 font-semibold">{project.title}</h6>
                         <p className="mb-4 text-neutral-400">{project.description}</p>
+
                         {project.technologies.map((technology, index) => (
-                            <span key={index} className="m-2 rounded bg-neutral-900 px-2 py-1 text-small font-medium text-purple-900">{technology}</span>
+                            <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-small font-medium text-purple-900">{technology}</span>
                         ))}
+
+                        {project.title === "Real-time Chat Application" && (
+                            <p className="mt-2 text-small font-light">
+                                Click <a className="font-medium text-purple-900" href="https://chatapp-t58c.onrender.com/">here</a> for live demo.
+                            </p>
+                        )}
 
                         
                     </motion.div>
