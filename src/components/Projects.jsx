9716c995990b6418cad7cb6867f8c89238c7195a
Sanish Suwal <sanishsuwal3@gmail.com>
2025-01-09 13:16:14 -0500

    another fix

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -27,18 +27,22 @@ const Projects = () => {
                          initial={{opacity: 0, x:100}}
                          transition={{duration: 1}}
                         className="flex flex-wrap w-full max-w-xl lg:w-3/4">
-                        <h6 className="mb-3 font-semibold">{project.title}</h6>
+                        <h6 className="mb-3 font-semibold">{project.title} 
+                            {project.title === "Real-time Chat Application" && (
+                                <a className="mx-6 font-extrabold text-purple-900" href="https://chatapp-t58c.onrender.com/">Demo</a>
+                            )}
+                        </h6>
                         <p className="mb-4 text-neutral-400">{project.description}</p>
 
                         {project.technologies.map((technology, index) => (
                             <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-small font-medium text-purple-900">{technology}</span>
                         ))}
 
-                        {project.title === "Real-time Chat Application" && (
+                        {/* {project.title === "Real-time Chat Application" && (
                             <p className="mt-2 text-small font-light">
                                 Click <a className="font-medium text-purple-900" href="https://chatapp-t58c.onrender.com/">here</a> for live demo.
                             </p>
-                        )}
+                        )} */}
 
                         
                     </motion.div>
