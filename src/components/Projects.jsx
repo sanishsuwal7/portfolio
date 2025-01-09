c4c873e57cf0a238915ad7fbf34cf66171b06fc6
Sanish Suwal <sanishsuwal3@gmail.com>
2025-01-09 13:17:53 -0500

    divssssss

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
         </motion.h1>
         <div>
             {PROJECTS.map((project, index) => (
-                <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+                <div key={index} className="mb-8 flex flex-wrap lg:justify-evenly">
                     <motion.div 
                          whileInView={{opacity: 1, x:0}}
                          initial={{opacity: 0, x:-100}}
